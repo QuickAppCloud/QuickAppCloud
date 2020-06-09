dccc02e429173ae05c6d203cfa3327ec20b4e74c
icell <2081648315@qq.com>
2020-06-09 15:29:41 +0800

    修改文件目录，增加一个测试路由，修改项目名称

--- src/router/index.js
+++ src/router/index.js
@@ -5,11 +5,19 @@ import HelloWorld from '@/components/HelloWorld'
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
   routes: [
     {
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('@/views/login.vue')
     }
+
   ]
 })
---
+++ src/views/login.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <p>这是一段文本</p>
+    </div>
+</template>
