c714a29147175128cd3cb66a77f24532683d2e14
icell <2081648315@qq.com>
2020-06-09 18:40:16 +0800

    暂存代码

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,5 +1,23 @@
 <template>
-    <div>
-        <p>这是一段文本</p>
+  <div class="loginRoot">
+    <div class="login">
     </div>
+  </div>
 </template>
+
+<style lang="scss">
+   .loginRoot{
+       background-image: url("../assets/login/bg.jpg");
+       height: 100%;
+       width:100%;
+       position: absolute;
+       background-size: cover;
+       z-index: -20;
+   }
+   .login{
+       background-image: url("../assets/login/u529.png");
+     // background-size: 100% 100%;
+     // font-size: 16px;
+      z-index:-1
+      }
+</style>
